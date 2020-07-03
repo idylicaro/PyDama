886e18930ebe0a8886b6f0baab3f38b13b188ba3
junio <junio.js977@gmail.com>
2020-07-03 20:39:19 -0300

    alterei em player1

--- pydama/src/controller/gameControl.py
+++ pydama/src/controller/gameControl.py
@@ -7,7 +7,7 @@ def initializeTabuleiro():
                 [0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0]]
-
+    
     for i in range(len(tabuleiro)):
         for j in range(len(tabuleiro[i])):
             tabuleiro[i][j] = 'x'
