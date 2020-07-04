5d30cb79059882230e8c81e41ebf524af502f5d4
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-04 15:40:05 -0300

    feat: melhorou apresentacao colocando coordenadas

--- pydama/src/view/presentation.py
+++ pydama/src/view/presentation.py
@@ -1,9 +1,13 @@
 
-whiteColor = ''
-
 def display (tabuleiro):
+    charCodeInitial = 65 #code asc2 
+
+    print(' ','','0','1','2','3','4','5','6','7')
     for i in range(len(tabuleiro)):
+        
         for j in range(len(tabuleiro[i])):
+            if (j==0):
+                print(chr(charCodeInitial),end='  ')
             if (i % 2!=0):
                 if(j % 2 == 0):
                     print(tabuleiro[i][j], end=' ')
@@ -15,6 +19,7 @@ def display (tabuleiro):
                 else:
                     print(coloredWhite(tabuleiro[i][j]), end=' ')
         print()
+        charCodeInitial += 1
 
 def coloredWhite(string):
     return '\x1b[0;30;47m' + string + '\x1b[0m'
