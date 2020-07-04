6be091f7756a2819c70e8cd449905a45e406ebf5
junio <junio.js977@gmail.com>
2020-07-03 21:27:58 -0300

    Mudei, o player só para teste

--- pydama/src/main.py
+++ pydama/src/main.py
@@ -2,7 +2,7 @@
 import view , controller
 from view import presentation
 from controller import gameControl
-player0 = 12
+player1 = 12
 player2 = 12
 tabuleiro = gameControl.initializeTabuleiro()
 presentation.display(tabuleiro)
