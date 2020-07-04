29ad5a185899d6cc9ee00ea3f6d76ca9edf70ab3
Lucas Andrey <lucas.andrey20@academico.ufs>
2020-07-03 22:34:10 -0300

    REFACTOR CODE

--- pydama/src/main.py
+++ pydama/src/main.py
@@ -4,8 +4,7 @@ from view import presentation
 from controller import gameControl
 player1 = 12
 player2 = 12
-while (player1 != 0 or player2 != 0):
-    tabuleiro = gameControl.initializeTabuleiro()
-    presentation.display(tabuleiro)
+#while (player1 != 0 or player2 != 0):
+tabuleiro = gameControl.initializeTabuleiro()
+presentation.display(tabuleiro)
 
-print('eu teste')
