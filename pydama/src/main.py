f022171fdc7789a047f74636a757cacb70b58d7a
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-03 21:41:14 -0300

    feat: create loop

--- pydama/src/main.py
+++ pydama/src/main.py
@@ -4,7 +4,8 @@ from view import presentation
 from controller import gameControl
 player1 = 12
 player2 = 12
-tabuleiro = gameControl.initializeTabuleiro()
-presentation.display(tabuleiro)
+while (player1 != 0 or player2 != 0):
+    tabuleiro = gameControl.initializeTabuleiro()
+    presentation.display(tabuleiro)
 
 
