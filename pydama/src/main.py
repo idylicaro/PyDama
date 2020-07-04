4641e149d8caff47ec7365b032affbc94e80de86
GuiMesc <36601466+GuiMesc@users.noreply.github.com>
2020-07-03 21:08:19 -0300

    acrescimo de player2

--- pydama/src/main.py
+++ pydama/src/main.py
@@ -3,6 +3,7 @@ import view , controller
 from view import presentation
 from controller import gameControl
 player0 = 0
+player2 = 12
 tabuleiro = gameControl.initializeTabuleiro()
 presentation.display(tabuleiro)
 
