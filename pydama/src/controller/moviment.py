4d88f4fdb1065d86e009c014f6d13fd1866599c9
Idyl Icaro dos Santos <45442467+idylicaro@users.noreply.github.com>
2020-07-25 16:44:16 -0300

    Delete moviment.py

--- pydama/src/controller/moviment.py
+++
@@ -1,9 +0,0 @@
-import os
-import gameControl
-
-def movimentPecaPreta(xorigem, yorigem, xlinha, ylinha, tabuleiro):
-    gameControl.getCoordenadas()
-    if tabuleiro[xorigem][yorigem] == 'P':
-        tabuleiro[xorigem][yorigem] = ' '
-        tabuleiro[xlinha][yorigem] = 'p'
-       
